f8cc96a170d56dc53d484e6b8cae4bafef59bc27
Letícia Chaves <leticiaschaves7@gmail.com>
2023-05-31 15:35:20 -0300

    add tailwind and logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 const username = document.getElementById('username');
 const password = document.getElementById('password');
 const submitBtn = document.getElementById('button');
+
+const incorrectLoginAlert = () => {
+    alert('please, type something...');
+}
+
+const correctLoginAlert = () => {
+    alert('nice username! :)');
+}
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+}
+
+function validateLogin() {
+    const invalidUsername = '';
+    const invalidPassword = '';
+    if (
+        username.value === invalidUsername &&
+        password.value === invalidPassword
+    ) {
+        incorrectLoginAlert();
+    }
+    return correctLoginAlert();
+}
+  
+  submitBtn.addEventListener('click', validateLogin);
+
+window.onload = function () {
+    submitBtn.addEventListener('click', handleSubmit);
+};
+
+const enableSubmit = () => {
+    submitBtn.disabled = this.value;
+}
+
+  window.onload = () => {
+    username.addEventListener('change', enableSubmit);
+    password.addEventListener('change', enableSubmit);
+  }
